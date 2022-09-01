267e847dbcd9bac2bd3272789714e4e677e997d3
Edgaras <105384015+EdgarasAnd@users.noreply.github.com>
2022-09-01 20:23:01 +0300

    test: first try

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+
+
+function playRound(playerSelection, computerSelection) {
+    
+  for (let i = 0; i < 5; i++) {
+    
+ }
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
