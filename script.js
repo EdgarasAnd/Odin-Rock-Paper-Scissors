a8355a8c14cf8440a8d71bf7a4ca0b687ccb5fe3
Edgaras <105384015+EdgarasAnd@users.noreply.github.com>
2022-09-05 22:07:00 +0300

    test: game function compares selections and gives the result once.

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
+const array = ["Rock", "Paper", "Scissors"];
+const computerSelection = array[Math.floor(Math.random() * array.length)];
 
+const playerSelection = prompt("Rock, paper, scissors");
 
-function playRound(playerSelection, computerSelection) {
-    
-  for (let i = 0; i < 5; i++) {
-    
- }
-  }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+function game() {
+  if (playerSelection === "Rock" && computerSelection === "Rock") {
+    console.log(`You both choise ${playerSelection} Result: Tie`);
+  } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+    console.log(
+      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: Tie`
+    );
+  } else if (
+    playerSelection === "Scissors" &&
+    computerSelection === "Scissors"
+  ) {
+    console.log(
+      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: Tie`
+    );
+  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    console.log(
+      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You WIN`
+    );
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    console.log(
+      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You WIN`
+    );
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    console.log(
+      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You WIN`
+    );
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    console.log(
+      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You WIN`
+    );
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    console.log(
+      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You LOOSE`
+    );
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    console.log(
+      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You LOOSE`
+    );
+  } else console.log("Error");
+}
+// function playRound(playerSelection, computerSelection) {
+
+//   return (`Player choise: ${playerSelection} ` + `PCU chose: ${computerSelection}`)
+// }
+
+// function game() {
+//     for (let i = 1; i <= 5; i++) {
+//       if ()
+//       }
+// }
+
+console.log(game());
