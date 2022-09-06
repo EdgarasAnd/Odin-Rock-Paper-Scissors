6ce39dc2f6a5696a174974c605310cf20efda9c0
Edgaras <105384015+EdgarasAnd@users.noreply.github.com>
2022-09-06 18:04:21 +0300

    test: try to count result

--- script.js
+++ script.js
@@ -1,56 +1,78 @@
 const array = ["rock", "paper", "scissors"];
 
-
-
 function playRound() {
   var computerSelection = array[Math.floor(Math.random() * array.length)];
-
   var playerSelection = prompt("Rock, paper, scissors");
-var playerSelection = playerSelection.toLowerCase()
-  if (playerSelection === "rock" && computerSelection === "rock") {
-    console.log(`You both choise ${playerSelection} Result: Tie`);
-  } else if (playerSelection === "paper" && computerSelection === "paper") {
-    console.log(
-      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: Tie`
-    );
-  } else if (
-    playerSelection === "scissors" &&
-    computerSelection === "scissors"
-  ) {
-    console.log(
-      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: Tie`
-    );
+  var playerSelection = playerSelection.toLowerCase();
+  var result = 0;
+  if (playerSelection === computerSelection) {
+    result = 'tie'
+    console.log(result);    
+  // } else if (playerSelection === "paper" && computerSelection === "paper") {
+  //   result = "Tie";
+  // } else if (
+  //   playerSelection === "scissors" &&
+  //   computerSelection === "scissors"
+  // ) {
+    // console.log(
+    //   `You choise ${playerSelection} and PCU choise ${computerSelection} Result: Tie`
+    // );
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    console.log(
-      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You WIN`
-    );
+    result = 'Loose'
+    console.log(result);  
+    // console.log(
+    //   `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You WIN`
+    // );
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    console.log(
-      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You WIN`
-    );
+    result = 'Win'
+    console.log(result);  
+    // console.log(
+    //   `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You WIN`
+    // );
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    console.log(
-      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You WIN`
-    );
+    result = 'Win'
+    console.log(result);  
+    // console.log(
+    //   `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You WIN`
+    // );
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    console.log(
-      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You WIN`
-    );
+    result = 'Win'
+    console.log(result);  
+    // console.log(
+    //   `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You WIN`
+    // );
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    console.log(
-      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You LOOSE`
-    );
+    result = 'Loose'
+    console.log(result);  
+    // console.log(
+    //   `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You LOOSE`
+    // );
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    console.log(
-      `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You LOOSE`
-    );
+    result = 'Loose'
+    console.log(result);  
+    // console.log(
+    //   `You choise ${playerSelection} and PCU choise ${computerSelection} Result: You LOOSE`
+    // );
   } else console.log("Error");
+  // return result
+}
+
+function game() {
+  var playerCount = 0
+  for (let i = 1; i <= 5; i++) {
+    playRound(i);
+    console.log(playerCount)
+  }
 }
 
 
-function game() {
-    for (let i = 1; i <= 5; i++) {
-      playRound(i)
-}}
 
+// function finalResult(){
+//   var playerCount = 0
+//   for (let i = 0; i + game(i); i++) {
+//     playerCount
+//   }
+// }
 console.log(game());
+
+
